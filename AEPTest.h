f22e70d93f39f33f112cb699fc1609c3e9a9bf9b
Abraham Escamilla Pinelo <Abraham@Abrahams-MacBook.local>
2014-09-02 19:44:18 -0500

    added a test class

---
+++ AEPTest.h
@@ -0,0 +1,13 @@
+//
+//  AEPTest.h
+//  Git Test
+//
+//  Created by Abraham Escamilla Pinelo on 02/09/14.
+//  Copyright (c) 2014 Abraham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AEPTest : NSObject
+
+@end
---
+++ AEPTest.m
@@ -0,0 +1,13 @@
+//
+//  AEPTest.m
+//  Git Test
+//
+//  Created by Abraham Escamilla Pinelo on 02/09/14.
+//  Copyright (c) 2014 Abraham. All rights reserved.
+//
+
+#import "AEPTest.h"
+
+@implementation AEPTest
+
+@end
